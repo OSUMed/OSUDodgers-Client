b3fd8c43dec70dc9c4826b95afef08b996a8238f
Srikanth Medicherla <mediches@oregonstate.edu>
2021-08-28 22:05:45 -0700

    NewVersion1

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import Axios from 'axios';
-import Records from "./Records";
-import Game from "./Game";
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
+import Records from "./components/Records";
+import Villain from "./Villain";
+import Records from "./components/Records";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 export default function App() {
+  // function RecordsPage (){
+  //   window.location.href = "./Records";
+  // }
 
-function RecordsPage (){ 
-  window.location.href = "./Records"; 
-}
-
-function MainPage (){ 
-  window.location.href = "./"; 
-}
+  // function MainPage (){
+  //   window.location.href = "./";
+  // }
 
   return (
-
     <div>
-      <Button color="primary" onClick={ MainPage }>
+      {/* <Button color="primary" onClick={ MainPage }>
       Game Page
     </Button>
     <Button color="secondary" onClick={ RecordsPage }>
     Records Page
-    </Button>
-      
+    </Button> */}
+
       <BrowserRouter>
-      
-      <Switch>
-        <Route exact from="/"> < Game/> </Route>
-        <Route exact from="/Game"> < Game/> </Route>
-        <Route exact from="/records"> < Records/> </Route>
-
-      </Switch>
+        <NavBar />
+        <Switch>
+          <Route path="/villain" component={Villain}></Route>
+          <Route path="/records" component={records}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/villain" />
+          <Redirect to="/not-found" />
+        </Switch>
       </BrowserRouter>
-
- 
     </div>
   );
 }
---
+++ src/components/notFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <h1>Page Cannot Be Found</h1>;
+};
+
+export default NotFound;
